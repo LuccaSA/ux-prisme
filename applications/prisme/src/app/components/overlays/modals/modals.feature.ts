9e8146ec106f41735ff979f9e1a3cd25de941d73
lucienbertin <me@lucienbertin.com>
2019-07-12 16:50:43 +0200

    rem older example of modal components

--- applications/prisme/src/app/components/overlays/modals/modals.feature.ts
+++ applications/prisme/src/app/components/overlays/modals/modals.feature.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { IFeatureInfos, ThemeDocumentationService } from '@prisme/common';
-import { BasicExample } from './basic/basic.example';
+import { IFeatureInfos } from '@prisme/common';
 declare var require: any;
 @Component({
 	selector: 'pri-modals',
@@ -8,19 +7,14 @@ declare var require: any;
 })
 export class ModalsFeature {
 	infos: IFeatureInfos = {
-		title: 'Dialogues',
+		title: 'Modales',
 		description: "Les fenêtres de dialogue vient superposer un contenu supplémentaire sans quitter la page en cours. Elles ont diverses utilités comme la création ou l'édition de contenu, la confirmation ou l'accès à des fonctionnalités avancées.",
 		examples: [
-			{
-				title: 'Basique',
-				component: BasicExample,
-				code: require('!!prismjs-loader?lang=markup!./basic/basic.example.html'),
-				tsCode: require('!!prismjs-loader?lang=typescript!./basic/basic.example.ts'),
-			},
+
 		],
 		packages: [
 			'SCSS'
 		],
 	};
-	constructor(private themeService: ThemeDocumentationService, ) {}
+	constructor() {}
 }
