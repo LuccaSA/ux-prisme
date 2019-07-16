3cec048e8e181f6815eef98e69193dca9ae8739b
lucienbertin <me@lucienbertin.com>
2019-07-16 10:38:53 +0200

    add page for department select and examples for basic and multiple

---
+++ applications/prisme/src/app/components/form/department-select/department-select.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@prisme/common';
+import { DepartmentSelectFeature } from './department-select.feature';
+import { BasicExample } from './basic/basic.example';
+import { FormsModule } from '@angular/forms';
+import { LuDepartmentModule } from '@lucca-front/ng';
+import { MultipleExample } from './multiple/multiple.example';
+
+@NgModule({
+	imports: [
+		CommonModule,
+		FormsModule,
+		LuDepartmentModule,
+	],
+	declarations: [
+	DepartmentSelectFeature,
+	BasicExample,
+	MultipleExample],
+	entryComponents: [
+	BasicExample,
+	MultipleExample]
+})
+export class DepartmentSelectModule {}
