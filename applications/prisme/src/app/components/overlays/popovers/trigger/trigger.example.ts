b2603400284b44cabb80e9fd9e849fa30584cd1a
jeremie-lucca <jbiron@lucca.fr>
2019-07-24 16:07:11 +0200

    [doc] popovers

--- applications/prisme/src/app/components/overlays/popovers/trigger/trigger.example.ts
+++ applications/prisme/src/app/components/overlays/popovers/trigger/trigger.example.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'pri-trigger-example',
-	templateUrl: './trigger.example.html'
+	templateUrl: './trigger.example.html',
+	styleUrls: ['./trigger.example.scss']
 })
 export class TriggerExample {
 }
