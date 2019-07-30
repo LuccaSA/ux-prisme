cb321848d44cf7712ccd055435ec9a7a94f5409c
jeremie-lucca <jbiron@lucca.fr>
2019-07-30 11:21:36 +0200

    [guidelines] callout

--- applications/prisme/src/app/components/feedback/callouts/callouts.feature.ts
+++ applications/prisme/src/app/components/feedback/callouts/callouts.feature.ts
@@ -16,6 +16,7 @@ export class CalloutsFeature {
 	infos: IFeatureInfos = {
 		title: 'Callouts',
 		packages: ['SCSS'],
+		guidelines: 'guidelines/components/feedback/callouts/callouts.guidelines.md',
 		description: "Les callouts servent à mettre en valeur une information dans une page, comme une aide, une alerte ou une erreur.",
 		examples: [
 			{
