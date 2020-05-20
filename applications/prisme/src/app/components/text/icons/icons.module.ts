9933b0ea9d5f9d781fc9ea5ebf4f2e93cf343c27
sanlucca <sgoldscheider@lucca.fr>
2020-05-20 12:44:25 +0200

    [add] New and prev icons

--- applications/prisme/src/app/components/text/icons/icons.module.ts
+++ applications/prisme/src/app/components/text/icons/icons.module.ts
@@ -4,6 +4,7 @@ import { IconsFeature } from './icons.feature';
 import { BasicExampleComponent } from './basic/basic.example';
 import { ColorsExampleComponent } from './colors/colors.example';
 import { SizeExampleComponent } from './size/size.example';
+import { PreviousExample } from './previous/previous.example';
 
 @NgModule({
 	imports: [CommonModule],
@@ -11,12 +12,14 @@ import { SizeExampleComponent } from './size/size.example';
 		IconsFeature,
 		BasicExampleComponent,
 		ColorsExampleComponent,
-		SizeExampleComponent
+		SizeExampleComponent,
+		PreviousExample
 	],
 	entryComponents: [
 		BasicExampleComponent,
 		ColorsExampleComponent,
-		SizeExampleComponent
+		SizeExampleComponent,
+		PreviousExample
 	]
 })
 export class IconsModule { }
