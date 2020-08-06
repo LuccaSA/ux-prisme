87b03282fccab6b4952bdcfcc170cb3458d7f40e
Nais Alcaraz Monteyrol <nsalcaraz@gmail.com>
2020-08-06 15:56:25 +0200

    Add documentation for label number

--- applications/prisme/src/app/components/text/labels/labels.module.ts
+++ applications/prisme/src/app/components/text/labels/labels.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@prisme/common';
 import { LabelsFeature } from './labels.feature';
 import { BasicExampleComponent } from './basic/basic.example';
+import { NumbersExample } from './numbers/numbers.example';
 
 @NgModule({
 	imports: [CommonModule],
 	declarations: [
 		LabelsFeature,
-		BasicExampleComponent
+		BasicExampleComponent,
+		NumbersExample
 	],
 	entryComponents: [
-		BasicExampleComponent
+		BasicExampleComponent,
+		NumbersExample
 	]
 })
 export class LabelsModule { }
