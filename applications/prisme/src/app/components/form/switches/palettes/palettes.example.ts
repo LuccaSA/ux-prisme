d1bde9be18821b25aed735192e0771d57755f303
Alexia Alarza <aalarza@lucca.fr>
2021-08-03 15:08:33 +0200

    [DS2LF] Switch

--- applications/prisme/src/app/components/form/switches/palettes/palettes.example.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'ds-palettes-example',
-	templateUrl: './palettes.example.html'
-})
-export class PalettesExampleComponent {
-	constructor() { }
-}
---
+++ applications/prisme/src/app/components/form/switches/sizes/sizes.example.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pri-sizes-example',
+  templateUrl: './sizes.example.html',
+  styleUrls: ['./sizes.example.css']
+})
+export class SizesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
