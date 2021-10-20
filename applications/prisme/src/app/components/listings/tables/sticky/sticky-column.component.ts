9a7305a807edd5bc924b7f0b05f6fbe3c5b83b1f
Sandy Goldscheider <sgoldscheider@lucca.fr>
2021-10-20 17:04:58 +0200

    [add] Doc for sticky elements in tables

---
+++ applications/prisme/src/app/components/listings/tables/sticky/sticky-column.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'ds-sticky-column-example',
+	templateUrl: './sticky-column.component.html',
+	styles: [":host {display: block; overflow: auto;}"]
+})
+export class StickyColumnExampleComponent {
+}
---
+++ applications/prisme/src/app/components/listings/tables/sticky/sticky-header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'ds-sticky-header-example',
+	templateUrl: './sticky-header.component.html',
+	styles: [":host {display: block; height: 200px; overflow: auto;}"]
+})
+export class StickyHeaderExampleComponent {
+}
